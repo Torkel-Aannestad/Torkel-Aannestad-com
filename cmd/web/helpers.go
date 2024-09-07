fa0792caf8f00d04b55fb01f9cff277fb06ea9ec
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-09-07 11:31:33 +0200

    response helpers

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,1 +1,19 @@
 package main
+
+import "net/http"
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	method := r.Method
+	uri := r.URL.RequestURI()
+	app.logger.Error("server error", "method", method, "uri", uri, "error", err)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientErrorResponse(w http.ResponseWriter, statusCode int) {
+	if statusCode < 400 || statusCode > 499 {
+		statusCode = 400
+	}
+
+	http.Error(w, http.StatusText(statusCode), statusCode)
+}
