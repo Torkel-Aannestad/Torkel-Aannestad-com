e44bed7fe6bbb9a7595cae6a8faec1f3998ea3ed
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-09-09 11:03:25 +0200

    basic hero & footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./**/*.html", "./**/*.tmpl"],
+    theme: {
+        extend: {
+          colors: {
+            lightBeige: '#FFF8E9'
+          }
+        }
+    }
 }
