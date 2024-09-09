f3bfada795671801d7aac2bf8bd0be6c50d28db2
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-09-09 13:03:47 +0200

    Font import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
     content: ["./**/*.html", "./**/*.tmpl"],
     theme: {
         extend: {
           colors: {
             lightBeige: '#FFF8E9'
-          }
+          },
+          fontFamily: {
+            'sans': ['"Urbanist"', ...defaultTheme.fontFamily.sans],
+        }
         }
     }
 }
