9cad35b16d1f201d6ac1aa6b38a46c8600508352
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-11 13:53:50 +0100

    Titles tags

--- cmd/web/posts.go
+++ cmd/web/posts.go
@@ -9,18 +9,11 @@ type post struct {
 
 func newPostsData() map[string]post {
 	posts := map[string]post{}
-	posts["post-design-system-something-test"] = post{
-		Title:       "Crafting a design system for a multiplanetary future",
-		Slug:        "post-design-system-something-test",
+	posts["how-this-website-is-made"] = post{
+		Title:       "How This Website is Made",
+		Slug:        "how-this-website-is-made",
 		Description: `Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system.`,
 		Date:        "September 5, 2022",
 	}
-	posts["tester-post"] = post{
-		Title:       "TESTER POST",
-		Slug:        "tester-post",
-		Description: `Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system.`,
-		Date:        "Oktober 15, 2022",
-	}
-
 	return posts
 }
