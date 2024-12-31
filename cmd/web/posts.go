22751fccbbde5f905693a96b43221cf044757160
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-12-31 16:53:07 +0100

    initial article draft

--- cmd/web/posts.go
+++ cmd/web/posts.go
@@ -11,6 +11,14 @@ type post struct {
 
 func newPostsData() map[string]post {
 	posts := map[string]post{}
+
+	posts["how-to-work-with-passwords-with-argon2-in-go"] = post{
+		Title:       "How to Work with Passwords with Argon2 in Go",
+		Slug:        "how-to-work-with-passwords-with-argon2-in-go",
+		Description: `A guide on how to hash, verify and encrypt password in go using argon2 for hashing and AES GCM algorithm for encryption.`,
+		Date:        "Desember, 2024",
+		RSSPubDate:  "2024-12-31T08:00:00Z",
+	}
 	posts["behind-the-scenes"] = post{
 		Title:       "Behind the Scenes: Building This Website",
 		Slug:        "behind-the-scenes",
